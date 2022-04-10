3d6aeb4ca12191c626ecbf3469177641d758ecb2
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2022-04-10 11:19:27 -0300

    C12 - Realizado todos os exercicios dia 12.1

---
+++ front_end/bloco_12_ciclo_de_vida_de_componentes_e_react_router/12.1/dog_exercise/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Dog from './Components/Dog';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return <Dog />;
+  }
+}
+
+export default App;
