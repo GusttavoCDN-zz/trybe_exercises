d37eac8f9a7a20f5036823faae2d4b5595475bd3
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2022-05-05 18:54:29 -0300

    feat: feito exercicio digimons api

--- front_end/bloco_14_testes_automatizados_com_react_testing_libary/14.2/mock_exercise/service.js
+++ front_end/bloco_14_testes_automatizados_com_react_testing_libary/14.2/mock_exercise/service.js
@@ -1,3 +1,15 @@
-const randomNumber = () => Math.random() * 100;
+fetch = require('node-fetch');
 
-export { randomNumber };
+const randomNumber = () => Math.floor(Math.random() * 100) + 1;
+const toUpper = (string) => string.toUpperCase();
+const returnFirstLetter = (string) => string[0];
+const stringJoin = (s1, s2) => `${s1}${s2}`;
+
+const fetchDog = async () => {
+  const response = await fetch("https://dog.ceo/api/breeds/image/random");
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
+
+module.exports = {randomNumber, toUpper, returnFirstLetter, stringJoin, fetchDog};
